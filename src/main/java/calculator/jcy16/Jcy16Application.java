e9a068cf0d33d980b764afe4654581b2aad33eab
ANASA <anasa@ANASAui-MacBookPro.local>
2023-07-25 23:04:54 +0900

    Add Spring Boot

---
+++ src/main/java/calculator/jcy16/Jcy16Application.java
@@ -0,0 +1,13 @@
+package calculator.jcy16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jcy16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jcy16Application.class, args);
+	}
+
+}
---
+++ src/test/java/calculator/jcy16/Jcy16ApplicationTests.java
@@ -0,0 +1,13 @@
+package calculator.jcy16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jcy16ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
